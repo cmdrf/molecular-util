65c517f97127d2c76d978feaca7d37b3ec643680
Fabian Herb <fabian@herb-clan.de>
2019-08-29 17:54:03 +0200

    Add utility classes from molecular-meshfile

---
+++ molecular/util/Math.h
@@ -0,0 +1,16 @@
+#ifndef MOLECULAR_MATH_H
+#define MOLECULAR_MATH_H
+
+namespace molecular
+{
+
+namespace Math
+{
+
+static constexpr double kPi_d = 3.1415926535897932384626433832795029;
+
+}
+
+} // namespace molecular
+
+#endif // MOLECULAR_MATH_H
