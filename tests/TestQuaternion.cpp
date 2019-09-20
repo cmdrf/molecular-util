8be475dec9bd4d9bdf256943dc5fdc632232121b
Fabian Herb <fabian@herb-clan.de>
2019-09-21 01:52:01 +0200

    molecular::testbed target

--- tests/TestQuaternion.cpp
+++ tests/TestQuaternion.cpp
@@ -1,11 +1,30 @@
 /*	TestQuaternion.cpp
-	Copyright 2016-2017 Fabian Herb
 
-	This file is part of Molecular Engine.
+MIT License
+
+Copyright (c) 2019 Fabian Herb
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 */
 
 #include <catch.hpp>
-#include "Matchers.h"
+#include <molecular/testbed/Matchers.h>
 #include <molecular/util/Quaternion.h>
 #include <molecular/util/Matrix3.h>
 #include <molecular/util/Math.h>
